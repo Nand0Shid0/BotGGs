25b332109c591e52d95bba09f717a743db5276ca
Nand0Shid0 <lolgurrola4545@gmail.com>
2022-03-30 11:33:42 -0600

    Loopback

--- inf.py
+++ inf.py
@@ -1,17 +1,14 @@
 from webex_bot.models.command import Command
-
 import json
 import requests
-requests.packages.urllib3.disable_warnings()
 
-api_url = "https://192.168.1.99/restconf/data/ietf-interfaces:interfaces"
+api_url = "https://sandbox-iosxe-recomm-1.cisco.com/restconf/data/ietf-interfaces:interfaces"
 
 headers = { "Accept": "application/yang-data+json",
  "Content-type":"application/yang-data+json"
  }
 
-basicauth = ("cisco", "cisco123!")
-
+basicauth = ("developer", "C1sco12345")
 
 
 class Hola(Command):
@@ -23,18 +20,25 @@ class Hola(Command):
     def execute(self, message, attachment_actions, activity):
         a = message
         print (a)
-        return "Hola Humano"
-
-
+        return "Hola Humano" + a
 
 class Interfaces (Command):
     def __init__(self):
         super().__init__(
             command_keyword="interfaces",
+        )
+    def execute(self, message, attachment_actions, activity):
+        resp =  requests.get(api_url, auth=basicauth, headers=headers, verify=False)
+        response_json = resp.json()
+        r = str(json.dumps(response_json, indent=4))
+        return r + "\n SERVIDO JEFE......."
+
+class loopback (Command):
+    def __init__(self):
+        super().__init__(
+            command_keyword="loopback",
             help_message="Get current weather conditions by ZIP code.",
         )
     def execute(self, message, attachment_actions, activity):
-        rresp =  requests.get(api_url, auth=basicauth, headers=headers, verify=False)
-        response_json = rresp.json()
-        r = str(json.dumps(response_json, indent=4))
-        return r
+        a = message
+        
